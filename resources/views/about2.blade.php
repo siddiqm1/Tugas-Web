18daa790bd1d7d16965a1f78ada49f98bc34de0c
siddiqm1 <moehammad.siddiq@gmail.com>
2024-06-23 17:28:45 +0700

    first commit

---
+++ resources/views/about2.blade.php
@@ -0,0 +1,57 @@
+@extends('layout.main-about')
+
+@section('container')
+<div class="container">
+
+    <div class="card mt-5">
+        <div class="content">
+            <div class="imgBx">
+                <img src="/img/sidiq.jpg" alt="">
+            </div>
+            <div class="contentBx">
+                <h4>Muhammad Sidiq</h4>
+                <h5>221401053</h5>
+            </div>
+            <div class="sci">
+                <a href="#"><i class="fa fa-google" aria-hidden="true"></i></a>
+                <a href="https://instagram.com/sidiqqm_?igshid=NzZlODBkYWE4Ng=="><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+            </div>
+        </div>
+    </div>
+    <div class="card mt-5">
+        <div class="content">
+            <div class="imgBx">
+                <img src="/img/Dewi_Ragil.jpg" alt="">
+            </div>
+            <div class="contentBx">
+                <h4>Dewi Ragil</h4>
+                <h5>221401091</h5>
+            </div>
+            <div class="sci">
+                <a href="#"><i class="fa fa-google" aria-hidden="true"></i></a>
+                <a href="https://instagram.com/de_ragil?igshid=MzMyNGUyNmU2YQ=="><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+            </div>
+        </div>
+    </div>
+    <div class="card mt-5">
+        <div class="content">
+            <div class="imgBx">
+                <img src="/img/aji.jpg" alt="">
+            </div>
+            <div class="contentBx">
+                <h4 style="font-size: 1.25rem;">Muhammad Al-Ghifari Lubis</h4>
+                <h5>2210401108</h5>
+            </div>
+            <div class="sci">
+                <a href="#"><i class="fa fa-google" aria-hidden="true"></i></a>
+                <a href="https://instagram.com/4lghif?igshid=OGQ5ZDc2ODk2ZA=="><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+@endsection
